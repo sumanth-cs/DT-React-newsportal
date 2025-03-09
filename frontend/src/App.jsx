3e46bd946496874ca8068a6e24ed47eaf949847d
sumanth-cs <sumanthshetty698@gmail.com>
2025-03-09 12:55:42 +0530

    Installed shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
     <div className="bg-blue-500 text-white p-4 text-xl font-bold">
       Tailwind is working! 🎉
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="bg-red-500">Click me</Button>
+    </div>
     </div>
   )
 }
