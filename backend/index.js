b25f9a963d6e59d16ba18360625998aa0c614c79
sumanth-cs <sumanthshetty698@gmail.com>
2025-03-10 19:50:19 +0530

    Database connected

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  });
 
 const app = express();
 
