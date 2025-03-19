6ce366d017c4a5a87b14de1428ae8351be55b6ac
sumanth-cs <sumanthshetty698@gmail.com>
2025-03-19 21:18:23 +0530

    admin field added to user model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,8 +54,10 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Wrong Credentials"))
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    )
     const { password: pass, ...rest } = validUser._doc
 
     res.status(200).cookie("access_token", token, { httpOnly: true }).json(rest)
