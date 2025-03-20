3f75819c3a1d28e488abecfe1b75050480ec8ae4
sumanth-cs <sumanthshetty698@gmail.com>
2025-03-20 19:24:57 +0530

    create post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        category: {
+            type: String,
+            default: "uncategorized",
+        },
+        image: {
+            type: String,
+            default:
+                "https://euaa.europa.eu/sites/default/files/styles/width_600px/public/default_images/news-default-big.png?itok=NNXAZZTc",
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+    },
+    { timestamps: true }
+)
+
+const Post = mongoose.model("Post", postSchema)
+
+export default Post
