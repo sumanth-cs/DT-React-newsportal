b19016811b6347834856ecbbfa9528dd8e45cb9d
sumanth-cs <sumanthshetty698@gmail.com>
2025-03-26 07:21:57 +0530

    Dashboard all posts with show more functionality

--- frontend/src/components/shared/DashboardPost.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DashboardPosts = () => {
-  return <div>DashboardPosts </div>;
-};
-
-export default DashboardPosts;
---
+++ frontend/src/components/shared/DashboardPosts.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Link } from "react-router-dom";
+import { useUser } from "@/context/userContext";
+
+const DashboardPosts = () => {
+  const { currentUser } = useUser();
+
+  const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+
+        const data = await res.json();
+
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+
+        if (data.posts.length < 1) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full p-3">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table>
+            {/* <TableCaption>A list of your published articles.</TableCaption> */}
+
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[200px]">Date Updated</TableHead>
+                <TableHead>Post Image</TableHead>
+                <TableHead>Post Title</TableHead>
+                <TableHead>Category</TableHead>
+                <TableHead>Delete</TableHead>
+                <TableHead>Edit</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            {userPosts.map((post) => (
+              <TableBody className="divide-y">
+                <TableRow key={post._id}>
+                  <TableCell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </TableCell>
+
+                  <TableCell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </TableCell>
+
+                  <TableCell>
+                    <Link to={`/post/${post.slug}`}>{post.title}</Link>
+                  </TableCell>
+
+                  <TableCell>{post.category}</TableCell>
+
+                  <TableCell>
+                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                      Delete
+                    </span>
+                  </TableCell>
+
+                  <TableCell>
+                    <Link
+                      to={`/update-post/${post._id}`}
+                      className="font-medium text-green-600 hover:underline cursor-pointer"
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-blue-700 self-center text-sm py-7 hover:underline cursor-pointer"
+            >
+              Show more
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+    </div>
+  );
+};
+
+export default DashboardPosts;
